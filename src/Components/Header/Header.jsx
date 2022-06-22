99b2c095ef282eabb8425dbfa1a535b2e0609889
Sidi ESSAADOUNI <essaadouni.s@gmail.com>
2022-06-22 16:44:41 +0200

    change navBar
    put the backoffice bitton on navBar and use navigate constant as a props

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 
 import { useNavigate } from 'react-router-dom'
-import  Button  from '@mui/material/Button'
+// import  Button  from '@mui/material/Button'
 
 
 export default function Header() {
@@ -11,13 +11,7 @@ export default function Header() {
   return (
 
     <div className='header'>
-      <Navbar />
-      <>
-        <div className='header'>
-          <h1>Header</h1>
-          <Button onClick={()=>navigate('/BackOffice')} variant="outlined">BackOffice</Button>
-        </div>
-      </>
+      <Navbar navigate={navigate}/>
     </div>
   )
 }
--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -4,11 +4,12 @@ import Nav from './Nav/Nav'
 import Profile from './Profile/Profile'
 import Search from './Search/Search'
 import { navigation } from '../../../Constants/navBar';
+import { Button } from '@mui/material'
 
 const home = navigation.pages[0];
 console.log(home);
 
-export default function Navbar() {
+export default function Navbar({navigate}) {
   return (
     <nav aria-label='top' className='Navbar'>
       <div className="NavbarDiv">
@@ -18,6 +19,7 @@ export default function Navbar() {
           <div className="lastChild">
             <Search />
             <Profile />
+            <Button onClick={()=>navigate('/BackOffice')} variant="outlined">BackOffice</Button>
           </div>
         </div>
       </div>
