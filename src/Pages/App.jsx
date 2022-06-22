9634d9f92f6f5d4d9856c1c4a593a8ef5097ff97
JimmyDmtrx <96940667+JimmyDmtrx@users.noreply.github.com>
2022-06-22 14:38:40 +0200

    back office button
    add useNavigate
    install of package material ui and react router dom

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import {  Routes, Route } from "react-router-dom";
 
 import Contact from './Contact/Contact';
 import Home from './Home/Home';
+import BackOffice from './BackOffice/Home.BackOffice'
 import NotFound from './NotFound/NotFound';
 
 export default function App() {
   return (
     <div className="index">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/404" element={<NotFound />} /> 
-      </Routes>      
+      
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/BackOffice" element={<BackOffice />} />
+          <Route path="/404" element={<NotFound />} /> 
+        </Routes>  
+      
+          
     </div>
   );
 }
