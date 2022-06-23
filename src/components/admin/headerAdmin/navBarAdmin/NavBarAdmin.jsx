1e6febedc4e0fc2c2f550f40c0d6c1b001f6d25a
Sidi ESSAADOUNI <essaadouni.s@gmail.com>
2022-06-23 16:55:06 +0200

    Update NavBarAdmin.jsx

--- src/components/admin/headerAdmin/navBarAdmin/NavBarAdmin.jsx
+++ src/components/admin/headerAdmin/navBarAdmin/NavBarAdmin.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { navigation } from '../../../../constants/navBar';
+import Logo from '../../../app/header/navBar/logo/Logo'
+
+const home = navigation.pages[0];
 
 export default function NavBarAdmin() {
   return (
     <nav aria-label='top' className='Navbar'>
       <div className="NavbarDiv">
-      
+        <Logo home={home} className="-ml-28"/>
       </div>
     </nav>
   )
