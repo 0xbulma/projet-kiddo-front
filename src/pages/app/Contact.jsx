062e1d8d86300e8407d092122e40c3267a8b8e3f
JimmyDmtrx <96940667+JimmyDmtrx@users.noreply.github.com>
2022-06-30 15:52:05 +0200

    skeleton

--- src/pages/app/Contact.jsx
+++ src/pages/app/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LoadingComponent from "../../components/shared/LoadingComponent";
+import Skelet from "../../components/shared/Skelet";
 
 export default function Contact() {
   return (
     <>
+      <Skelet />
       <LoadingComponent />
       <div>Contact</div>
     </>
