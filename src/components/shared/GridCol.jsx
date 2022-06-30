cfcf16720bba86a627d363ef482e22954541c1df
Ben A <benjamin.anoufa@gmail.com>
2022-06-30 16:37:22 +0200

    Category working on Pagination

--- src/components/shared/GridCol.jsx
+++ src/components/shared/GridCol.jsx
@@ -1,12 +1,12 @@
 export function GridCol3(props) {
   return (
-    <div className={`grid grid-cols-3 gap-2 ${props.className}`}>{props.children}</div>
+    <div className={`grid grid-cols-3 gap-6 ${props.className}`}>{props.children}</div>
   )
 }
 
 export function GridCol2(props) {
   return (
-    <div className={`grid grid-cols-2 gap-2 ${props.className}`}>{props.children}</div>
+    <div className={`grid grid-cols-2 gap-6 ${props.className}`}>{props.children}</div>
   )
 }
 
