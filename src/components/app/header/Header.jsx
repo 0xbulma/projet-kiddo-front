9a866190977a8b7c5ea45faccda83bfac0c780d4
Étienne S <sauvage-etienne@orange.fr>
2022-07-06 17:16:53 +0200

    Adjust home and navbar design

--- src/components/app/header/Header.jsx
+++ src/components/app/header/Header.jsx
@@ -1,15 +1,15 @@
-import { Popover } from '@headlessui/react'
+import { Popover } from '@headlessui/react';
 
-import Navbar from "./navbar/Navbar"
+import Navbar from './navbar/Navbar';
 
-import './_header.css'
+import './_header.css';
 
 export let headerMargin = 'mb-0';
 
 export default function Header() {
   return (
-    <Popover className={`header ${headerMargin}`} >
+    <Popover className={`header ${headerMargin}`}>
       <Navbar margin={headerMargin} />
     </Popover>
-  )
+  );
 }
