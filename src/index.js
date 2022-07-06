9b1c1b023d1f9abadbc5a0ae9b903e2aacab112c
JimmyDmtrx <96940667+JimmyDmtrx@users.noreply.github.com>
2022-07-06 09:53:18 +0200

    user page

--- src/index.js
+++ src/index.js
@@ -1,57 +1,82 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { ApolloProvider } from '@apollo/client';
-import reportWebVitals from './reportWebVitals';
-import { apollo } from './graphql/apollo';
+import { ApolloProvider } from "@apollo/client";
+import reportWebVitals from "./reportWebVitals";
+import { apollo } from "./graphql/apollo";
 
-import UserLayout from './pages/layout/UserLayout';
-import AdminLayout from './pages/layout/AdminLayout';
+import UserLayout from "./pages/layout/UserLayout";
+import AdminLayout from "./pages/layout/AdminLayout";
 
 //App layout components
 
-import Home from './pages/app/Home';
-import Kiddo from './pages/app/Kiddo';
-import Contact from './pages/app/Contact.jsx';
-import NotFound from './pages/app/NotFound.jsx';
+import Home from "./pages/app/Home";
+import Kiddo from "./pages/app/Kiddo";
+import Contact from "./pages/app/Contact.jsx";
+import NotFound from "./pages/app/NotFound.jsx";
 
-import CategoryPage from './pages/app/CategoryPage';
-import EventPage from './pages/app/EventPage';
+import CategoryPage from "./pages/app/CategoryPage";
+import EventPage from "./pages/app/EventPage";
 
 //Admin layout components
 
-import AdminDashboard from './pages/administration/Dashboard';
-import AdminUser from './pages/administration/sections/user/User';
-import AdminUserProfil from './pages/administration/sections/user/UserProfil';
-import AdminUserTestMutation from './pages/administration/sections/user/UserTestMutation';
+import AdminDashboard from "./pages/administration/Dashboard";
+import AdminUser from "./pages/administration/sections/user/User";
+import AdminUserProfil from "./pages/administration/sections/user/UserProfil";
+import AdminUserTestMutation from "./pages/administration/sections/user/UserTestMutation";
 
-import './global.css';
-import Signalement from './pages/administration/sections/Report';
+import "./global.css";
+import Signalement from "./pages/administration/sections/Report";
+import UserInfo from "./pages/app/UserInfo";
 
 let isAdmin = true;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   // <React.StrictMode>
   <ApolloProvider client={apollo}>
     <Router>
       <Routes>
-        <Route path='/' element={<UserLayout composant={<Home />} />} />
-        <Route path='/kiddo' element={<UserLayout composant={<Kiddo />} />} />
-        <Route path='/contact' element={<UserLayout composant={<Contact />} />} />
-        {/* <Route path='/events' element={<UserLayout composant={<Events />} />} /> */}
-        <Route path='/event/:eventId' element={<UserLayout composant={<EventPage />} />} />
-        <Route path='/category/:category' element={<UserLayout composant={<CategoryPage />} />} />
-        <Route path='*' element={<UserLayout composant={<NotFound />} />} />
+        <Route path="/" element={<UserLayout composant={<Home />} />} />
+        <Route path="/kiddo" element={<UserLayout composant={<Kiddo />} />} />
+        <Route
+          path="/contact"
+          element={<UserLayout composant={<Contact />} />}
+        />
+        <Route path="/user" element={<UserLayout composant={<UserInfo />} />} />
+        <Route
+          path="/event/:eventId"
+          element={<UserLayout composant={<EventPage />} />}
+        />
+        <Route
+          path="/category/:category"
+          element={<UserLayout composant={<CategoryPage />} />}
+        />
+        <Route path="*" element={<UserLayout composant={<NotFound />} />} />
         {isAdmin && (
           <Fragment>
-            <Route path='/administration' element={<AdminLayout composant={<AdminDashboard />} />} />
-            <Route path='/administration/users' element={<AdminLayout composant={<AdminUser />} />} />
-            <Route path='/administration/users/:id' element={<AdminLayout composant={<AdminUserProfil />} />} />
-            <Route path='/administration/reports' element={<AdminLayout composant={<Signalement />} />} />
-            <Route path='/administration/userTest' element={<AdminLayout composant={<AdminUserTestMutation />} />} />
+            <Route
+              path="/administration"
+              element={<AdminLayout composant={<AdminDashboard />} />}
+            />
+            <Route
+              path="/administration/users"
+              element={<AdminLayout composant={<AdminUser />} />}
+            />
+            <Route
+              path="/administration/users/:id"
+              element={<AdminLayout composant={<AdminUserProfil />} />}
+            />
+            <Route
+              path="/administration/reports"
+              element={<AdminLayout composant={<Signalement />} />}
+            />
+            <Route
+              path="/administration/userTest"
+              element={<AdminLayout composant={<AdminUserTestMutation />} />}
+            />
           </Fragment>
         )}
 
---
+++ src/pages/app/UserInfo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserInfo = () => {
+  return (
+    <>
+      <div>UserInfo</div>
+    </>
+  );
+};
+
+export default UserInfo;
