9e513a5adf15e1de89ebe6e9ac3c45faaf47a345
Étienne S <sauvage-etienne@orange.fr>
2022-07-06 16:01:51 +0200

    Fix useless import

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { ApolloProvider } from "@apollo/client";
-import reportWebVitals from "./reportWebVitals";
-import { apollo } from "./graphql/apollo";
+import { ApolloProvider } from '@apollo/client';
+import reportWebVitals from './reportWebVitals';
+import { apollo } from './graphql/apollo';
 
-import UserLayout from "./pages/layout/UserLayout";
-import AdminLayout from "./pages/layout/AdminLayout";
+import UserLayout from './pages/layout/UserLayout';
+import AdminLayout from './pages/layout/AdminLayout';
 
 import AuthContext from './context/AuthContext';
 
 //App layout components
 
-import Home from "./pages/app/Home";
-import Kiddo from "./pages/app/Kiddo";
-import Contact from "./pages/app/Contact.jsx";
-import NotFound from "./pages/app/NotFound.jsx";
+import Home from './pages/app/Home';
+import Kiddo from './pages/app/Kiddo';
+import Contact from './pages/app/Contact.jsx';
+import NotFound from './pages/app/NotFound.jsx';
 
-import CategoryPage from "./pages/app/CategoryPage";
-import EventPage from "./pages/app/EventPage";
+import CategoryPage from './pages/app/CategoryPage';
+import EventPage from './pages/app/EventPage';
 
 //Admin layout components
 
-import AdminDashboard from "./pages/administration/Dashboard";
-import AdminUser from "./pages/administration/sections/user/User";
-import AdminUserProfil from "./pages/administration/sections/user/UserProfil";
-import AdminUserTestMutation from "./pages/administration/sections/user/UserTestMutation";
+import AdminDashboard from './pages/administration/Dashboard';
+import AdminUser from './pages/administration/sections/user/User';
+import AdminUserProfil from './pages/administration/sections/user/UserProfil';
+import AdminUserTestMutation from './pages/administration/sections/user/UserTestMutation';
 
-import Signalement from "./pages/administration/sections/Report";
-import UserInfo from "./pages/app/UserInfo";
+import Signalement from './pages/administration/sections/Report';
+// import UserInfo from "./pages/app/UserInfo";
 
 // Import CSS
 import './style.css';
 
 let isAdmin = true;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // <React.StrictMode>
