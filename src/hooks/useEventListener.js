3617d7be0c0ac19a25d75a6ad4f75d3208aa7b87
Ben A <benjamin.anoufa@gmail.com>
2022-07-08 17:49:00 +0200

    fixed unused

--- src/hooks/useEventListener.js
+++ src/hooks/useEventListener.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import  { useEffect, useRef } from "react";
 
 const useEventListener = (eventName, handler, element = window) => {
   const savedHandler = useRef();
