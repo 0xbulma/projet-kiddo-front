518935dd8dffa934bb680ac373616ed7d9130300
sessaadouni <essaadouni.s@gmail.com>
2022-07-11 00:39:34 +0200

    change

--- src/components/app/header/Header.jsx
+++ src/components/app/header/Header.jsx
@@ -2,13 +2,15 @@ import { Popover } from '@headlessui/react';
 import Menu from './navbar/Menu';
 
 import Navbar from './navbar/Navbar';
+import Navbar2 from './navbar2/Navbar2';
 
 import './_header.css';
 
 export default function Header() {
   return (
-    <Popover className={`header`}>
-      <Navbar />
+    <Popover className="header">
+      <Navbar /> 
+      <Navbar2 />
       <Menu />
     </Popover>
   );
