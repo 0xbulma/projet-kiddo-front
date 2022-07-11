c192f9733ff68b7188c9fb6b5097d5ae24ac97f5
Étienne S <sauvage-etienne@orange.fr>
2022-07-11 14:17:42 +0200

    Revert "Nav bar done & search in a new context"

--- src/pages/layout/UserLayout.jsx
+++ src/pages/layout/UserLayout.jsx
@@ -2,7 +2,6 @@ import Footer from '../../components/app/footer/Footer';
 import Navbar2 from '../../components/app/header/navbar2/Navbar2';
 
 export default function UserLayout({ composant }) {
-
   return (
     <>
       <Navbar2 />
