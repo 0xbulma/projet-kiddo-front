c15ca5da01cf4682af6c5ac35c7528c5a194a02c
Ben A <benjamin.anoufa@gmail.com>
2022-07-12 17:14:01 +0200

    fixed NAvBar

--- src/pages/layout/UserLayout.jsx
+++ src/pages/layout/UserLayout.jsx
@@ -1,13 +1,22 @@
 import Footer from '../../components/app/footer/Footer';
-// import Navbar2 from '../../components/app/header/navbar2/Navbar2';
-import Header from '../../components/app/header/Header'
-
+import Navbar2 from '../../components/app/header/navbar2/Navbar2';
+import Header from '../../components/app/header/Header';
+import { useState } from 'react';
+import useEventListener from '../../hooks/useEventListener';
 
 export default function UserLayout({ composant }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const windowSizeHandler = () => {
+    setIsMobile(window.innerWidth < 767);
+  };
+
+  useEventListener('resize', windowSizeHandler);
+
   return (
     <>
-      {/* <Navbar2 /> */}
-      <Header /> 
+      {isMobile && <Header />}
+      {!isMobile && <Navbar2 />}
       <div>{composant}</div>
       <Footer />
     </>
