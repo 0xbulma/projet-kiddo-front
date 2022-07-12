1be6b1f544e5010a54858c23b577526b299dbcf3
Étienne S <sauvage-etienne@orange.fr>
2022-07-12 14:26:39 +0200

    Page Kiddo (Non responsive)

--- src/pages/app/Kiddo.jsx
+++ src/pages/app/Kiddo.jsx
@@ -1,48 +1,109 @@
+import kiddoLogo from '../../assets/images/logo.svg';
+
+import img1 from '../../assets/images/kiddo/kiddo_img_1.png';
+import img2 from '../../assets/images/kiddo/kiddo_img_2.png';
+import img3 from '../../assets/images/kiddo/kiddo_img_3.png';
+import img4 from '../../assets/images/kiddo/kiddo_img_4.png';
+import img5 from '../../assets/images/kiddo/kiddo_img_5.png';
+
 export default function Kiddo() {
   return (
     <>
-      <section className='min-h-screen pt-28'>
-        <h2>Page Kiddo trop jolie</h2>
-      </section>
-      {/* <section className='generic-container'>
+      <section className='generic-container min-h-screen pt-28'>
         <img src={kiddoLogo} alt='Logo Kiddo' className='mt-20 mb-5 w-72' />
-        <article className='section__grid-3'>
-          <div className='col-span-2'>
+        {/* 1ère section */}
+        <article className='section__grid-3 pb-52'>
+          <div className='col-span-2  z-10'>
             <h2 className='font-bold mb-2'>
-              Des activités <br />
-              pour toutes les familles
+              Kiddo une plateforme créée <br />
+              pour les parents par les parents
             </h2>
-            <h3 className='text-kiddoGreen font-bold'>
-              Kiddo une plateforme d'activités <br />
-              pour les familles créée pour les parents
-            </h3>
             <p>
-              Elle vous propose une multitude d'activités à proximité de chez vous. Vous pouvez choisir parmi les diverses catégories votre activité.
-              Chaque famille peut choisir où organiser des activités culturelles, artistiques, sportives où manuelles. A vous d'imaginer où de
-              proposer des activités où des sorties avec vos enfants où avec d'autres familles..
+              Kiddo est une plateforme communautaire qui met en relation les parents pour partager ou organiser des activités familiales
+              extra-scolaires (sportives, culturelles, artistiques, manuelles, d’éveil corporel et autres), de proximité, dans le but de favoriser les
+              échanges entre les familles. A vous d'imaginer et de proposer des activités ou des sorties originales à partager avec vos enfants et
+              d'autres familles !
             </p>
           </div>
           <div>
-            <img src='https://picsum.photos/460/460?random' alt='' className='relative z-10 rounded-tl-3xl rounded-br-3xl' />
+            <img src={img1} alt='' className='absolute rounded-tl-3xl rounded-br-3xl -mt-52 -ml-32' />
           </div>
         </article>
-
-        <Circle color='bg-red-300' size='36' />
-        <article className='mt-12'>
-          <h2 className=''>Des activités pour toutes les familles</h2>
-          <h3 className=''>Kiddo la plateforme créer pour les parents par les parents</h3>
-          <div className={imgTextStyle}>
-            <div className='relative'>
-              <span className='bg-red-300 p-36 rounded-full absolute right-0 z-0'></span>
-              <img src='https://picsum.photos/600/400?random' alt='' className='relative z-10' />
-            </div>
-            <p className='mt-10'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, repellat? Earum tenetur doloremque et quaerat reprehenderit, aliquam
-              ea quos est id blanditiis corrupti illum? Odio quasi quos obcaecati. Adipisci, repudiandae!
+        {/* 2ème section */}
+        <article className='section__grid-3 py-52'>
+          <div>
+            <img src={img2} alt='' className='absolute z-0 rounded-tl-3xl rounded-br-3xl mt-32' />
+          </div>
+          <div className='col-span-2 text-right z-10'>
+            <h2 className='font-bold mb-2'>
+              Comment profiter
+              <br /> de la plateforme Kiddo ?
+            </h2>
+            <h3 className='text-purple-800 mb-10'>Kiddo s'addresse à toutes les familles</h3>
+            <p>
+              Kiddo est une plateforme communautaire qui met en relation les parents pour partager ou organiser des activités familiales
+              extra-scolaires (sportives, culturelles, artistiques, manuelles, d’éveil corporel et autres), de proximité, dans le but de favoriser les
+              échanges entre les familles. A vous d'imaginer et de proposer des activités ou des sorties originales à partager avec vos enfants et
+              d'autres familles !
             </p>
           </div>
         </article>
-      </section> */}
+        {/* 3ème section */}
+        <article className='section__grid-3 py-72'>
+          <div className='col-span-2 z-10'>
+            <h2 className='font-bold mb-2'>Vous pouvez participer à des activités à proximité de chez vous</h2>
+            <h3 className='text-red-600 mb-10'>
+              Inscrivez-vous à de nouvelles activités
+              <br />
+              originales, à vivre en famille !
+            </h3>
+            <p>
+              Qu’elles soient sportives, artistiques, culturelles, ou autres, vous trouverez très certainement l’activité qui vous conviendra ainsi
+              qu’à vos enfants. Vous n’aurez plus qu’à activer la géolocalisation.
+            </p>
+          </div>
+          <div>
+            <img src={img3} alt='' className='absolute z-0 rounded-tl-3xl rounded-br-3xl -mt-52 -ml-60' />
+          </div>
+        </article>
+        {/* 4ème section */}
+        <article className='section__grid-3 py-60'>
+          <div>
+            <img src={img4} alt='' className='absolute z-0 rounded-tl-3xl rounded-br-3xl' />
+          </div>
+          <div className='col-span-2 mr-28 text-right z-10'>
+            <h2 className='font-bold mb-2'>Vous pouvez également organiser des activités</h2>
+            <h3 className='text-green-800 mb-10'>Organisez des activités pour les enfants et les familles</h3>
+            <p>
+              Vous avez vécu de belles expériences en famille ? Partagez les en les organisant à votre tour sur la plateforme Kiddo et ainsi en faire
+              profiter d’autres familles !
+            </p>
+          </div>
+        </article>
+        {/* 5ème section */}
+        <article className='z-10 section__grid-3 py-96'>
+          <div className='col-span-2 z-10'>
+            <h2 className='font-bold mb-2 flex'>
+              La story <img src={kiddoLogo} alt='Logo Kiddo' className='w-52 -mt-3' />
+            </h2>
+            <h3 className='text-purple-800 mb-10'>Kiddo la plateforme créée pour les parents par les parents</h3>
+            <p>
+              • Kiddo est né d'un constat de familles qui ne trouvaient pas de site ou de plateforme collaborative proposant et regroupant à la fois
+              des activités extra-scolaires originales, ludiques, éducatives et de proximité pour leurs enfants de 3 à 12 ans.
+              <br />
+              <br />
+              • Ce constat est à l’origine de la création de la plateforme communautaire Kiddo.
+              <br />
+              <br />• Kiddo permet à des parents de proposer des activités à partager avec d’autres familles pour créer d’avantages de liens sociaux.
+              <br />
+              <br />• Kiddo facilite la recherche d’activités originales et récréatives à faire en famille autour de soi.
+            </p>
+          </div>
+          <div>
+            <img src={img5} alt='' className='z-0 absolute rounded-tl-3xl rounded-br-3xl -ml-60 -mt-10' />
+          </div>
+        </article>
+      </section>
     </>
   );
 }
